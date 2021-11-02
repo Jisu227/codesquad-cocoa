7d4947cfab2c2ae1c3c473308ab9ee52ea6c4dc0
jisu <jiiisu0227@gmail.com>
2021-11-02 23:45:09 +0900

    🎨 Implement the getArea function

--- Day1(21.11.02)/getArea.js
+++ Day1(21.11.02)/getArea.js
@@ -1,46 +1,23 @@
 // 1. getArea() 메서드를 구현해보자.
 
 function getArea(shape) {
+  let executionResult = [];
+  let executionOrder = [];
   if(shape === 'circle') {
-    console.log('원의 넓이는 '+calcCircle(10)+' 입니다.');
-  }else if(shape === 'ract') {
-    console.log('사각형의 넓이는 '+calcRact(10,15)+' 입니다.');
-  }else if(shape === 'trapezoid') {
-    console.log('사다리꼴의 넓이는 '+calcTrapezoid(10,15,12)+ ' 입니다.');
-  }else if(shape === 'sumCircle') {
-    console.log('모든 원의 넓이의 합은 '+ sumOfAllCircle(1,10) + ' 입니다.');
+    console.log(calcCircle(arguments[1],arguments[2]));
+    executionOrder.push('calcCircle');
+    executionResult.push(calcCircle(arguments[1],arguments[2]));
+
+  } else if(shape === 'ract') {
+    console.log(calcRactangle(arguments[1],arguments[2]));
+    executionOrder.push('calcRactangle');
+    executionResult.push(calcRactangle(arguments[1],arguments[2]));
+
+  } else if(shape === 'trapezoid') {
+    console.log(calcTrapezoid(arguments[1],arguments[2],arguments[3]));
+    executionOrder.push('calcTrapezoid');
+    executionResult.push(calcTrapezoid(arguments[1],arguments[2],arguments[3]));
   }
 }
 
-let shape = ['circle','ract','trapezoid','sumCircle'];
-for(let i = 0; i < shape.length; i++) {
-  getArea(shape[i]);
-}
-
-
-function calcCircle(radius) {
-  const area = radius * radius * 3.14;
-  return area;
-}
-
-function calcRact(width,height) {
-  const area = width * height;
-  return area;
-}
-
-function calcTrapezoid(upperSide,lowerSide,height) {
-  const area = (upperSide + lowerSide) * height;
-  return area;
-}
-
-function sumOfAllCircle(startRadius, endRadius) {
-  let area = 0;
-  for(let i = startRadius; i <= endRadius; i++) {
-    area += (i * i) * 3.14;
-  }
-  return area;
-}
-
-
-// 2. printExecutionSequence 함수만들기.
 
