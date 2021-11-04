d4887e64f6d0287dc08d31c3a07582a6048420f4
jisu <jiiisu0227@gmail.com>
2021-11-04 13:21:56 +0900

    ✨  Find the average score and print it out

--- mission 2/배열다루기.js
+++ mission 2/배열다루기.js
@@ -11,7 +11,21 @@ function calculate(m) {
   return result;
 }
 
+// 3. 평균구하기
+
+function calcGrades(grades) {
+  let result = '';
+  let sum = 0;
+  for(let i = 0; i < grades.length; i++) { // 각 학생의 평균점수구하고 출력하기
+    sum = grades[i].reduce((a,b) => a + b) / grades[i].length;
+    result += `${i+1}번째 학생의 평균점수는 ${sum.toFixed(2)} 입니다. \n`;
+  }
+  return result;
+}
 
 // test
 
 console.log(calculate(4));
+
+const grades = [[88,76,77], [33,44,44], [90,100,94], [30,44,98]];
+console.log(calcGrades(grades))
