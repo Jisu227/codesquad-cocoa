1673b95bbbc7f3c2bf59237758c04d1d6cea2997
jisu <jiiisu0227@gmail.com>
2021-11-16 01:51:00 +0900

    ✨  Implement the getStandardDeviation function

--- mission 5/성적관리 프로그램(함수형).js
+++ mission 5/성적관리 프로그램(함수형).js
@@ -13,7 +13,17 @@ function getVariance(data) { // 표준편차를 위한 분산
   return variance;
 }
 
+
+// 1-2.
+function getStandardDeviation() {
+  const variance = getVariance(data);
+  const standardDeviation = Math.sqrt(variance);
+
+  return standardDeviation;
+}
+
 // test 
 const data = [89.23, 82.03, 71.56, 78.82, 85.05, 84.44, 67.53, 71.7, 77.97, 73.77, 84.25, 67.01, 73.78, 64.19, 89.89, 90.32, 73.21, 75.35, 83.22, 74.01];
 console.log(getMean(data));
 console.log(getVariance(data));
+console.log(getStandardDeviation());
